1cb19427832dd51c0dbb3b736b7897d561fd615a
LuizFelipe406 <luizfpereira406@gmail.com>
2022-11-15 18:37:29 -0300

    setup app e server

---
+++ backend/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+
+    this.app.use(ErrorMiddleware.handle)
+  }
+
+  private config():void {
+    this.app.use(express.json());
+    this.app.use(cors({
+      origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+    }))
+  }
+
+  public start(PORT: string): void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+// A execução dos testes de cobertura depende dessa exportação
+export const { app } = new App();
