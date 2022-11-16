838ff9826b2e2e88699b9eb018c0a157805b54e0
LuizFelipe406 <luizfpereira406@gmail.com>
2022-11-16 09:28:38 -0300

    Fix: arruma get para post no transaction router

--- backend/src/routers/TransactionRouter.ts
+++ backend/src/routers/TransactionRouter.ts
@@ -17,6 +17,6 @@ export default class UserRouter {
 
   private configRoutes() {
     this.router.use(this.authMiddleware.validateToken);
-    this.router.get('/', (req, res, next) => this.transactionController.transfer(req, res, next));
+    this.router.post('/', (req, res, next) => this.transactionController.transfer(req, res, next));
   }
 }
