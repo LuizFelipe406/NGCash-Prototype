77ec18d110c85c576f93c433de70248241125722
LuizFelipe406 <luizfpereira406@gmail.com>
2022-11-18 13:53:21 -0300

    Botão de Logout

--- frontend/src/pages/Home/UserInfo/index.tsx
+++ frontend/src/pages/Home/UserInfo/index.tsx
@@ -1,27 +1,44 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import Context from "../../../context";
-import './UserInfo.css';
+import "./UserInfo.css";
 
 function UserInfo() {
   const { user } = useContext(Context);
-    return (
-      <div>
-        <h1 className="title">
-          perfil <AiOutlineArrowLeft />
-        </h1>
-        <div className="text profile-container white-box mb-5">
-          <span>{ user && `@${user.username}`}</span>
-        </div>
-        <h1 className="title">
-          carteira <AiOutlineArrowLeft />
-        </h1>
-        <div className="wallet-container text mb-5">
-          <span className="ms-3">saldo disponível</span>
-          <span className="me-3">{user && `R$ ${user.account.balance.toFixed(2)}`}</span>
-        </div>
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.setItem('token', '');
+    navigate('/');
+  }
+
+  return (
+    <div className="user-info-container">
+      <h1 className="title">
+        perfil <AiOutlineArrowLeft />
+      </h1>
+      <div className="text profile-container white-box mb-3">
+        <span>{user && `@${user.username}`}</span>
       </div>
-    );
+      <button
+        onClick={ logout }
+        type="button"
+        className="white-box text profile-logout-btn mb-3"
+      >
+        Sair
+      </button>
+      <h1 className="title">
+        carteira <AiOutlineArrowLeft />
+      </h1>
+      <div className="wallet-container text mb-5">
+        <span className="ms-3">saldo disponível</span>
+        <span className="me-3">
+          {user && `R$ ${user.account.balance.toFixed(2)}`}
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default UserInfo
+export default UserInfo;
